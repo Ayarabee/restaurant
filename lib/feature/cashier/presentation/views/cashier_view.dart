cf73029d88a7b2e47bf30f2093d56943bdb905eb
a-saber <ahs629862@gmail.com>
2023-09-24 02:45:17 +0300

    init cashier_view.dart

--- lib/feature/cashier/presentation/views/cashier_view.dart
+++ lib/feature/cashier/presentation/views/cashier_view.dart
@@ -1,11 +1,22 @@
 
 import 'package:flutter/material.dart';
+import 'package:restaurant/core/core_widgets/default_body/default_body.dart';
 
 class NewOrderView extends StatelessWidget {
   const NewOrderView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: DefaultBody(
+        title: 'Cashier',
+        body: Column(
+          children:
+          [
+            Text('data')
+          ],
+        )
+      ),
+    );
   }
 }
