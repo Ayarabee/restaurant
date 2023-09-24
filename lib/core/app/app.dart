dbccef9ba3eff8a35405f2667e5fcd0f7b649b82
ayara <ayarabee406@gmail.com>
2023-09-24 14:38:36 +0200

    update kitchen

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -3,25 +3,25 @@ import 'package:get/get.dart';
 import 'package:restaurant/core/local_database/cache_data.dart';
 import 'package:restaurant/core/localization/app_localization.dart';
 import 'package:restaurant/core/resources_manager/constants_manager.dart';
-import 'package:restaurant/feature/kitchen_view/presentation/views/kitchen_view.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp._internal();
-// kitchen
-  //kitchen 2
+
   static const MyApp _instance = MyApp._internal(); // singleton
   factory MyApp() => _instance; // factory
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: ConstantsManager.appTitle,
-        theme: ThemeData(
-          fontFamily: ConstantsManager.fontFamily,
-        ),
-        debugShowCheckedModeBanner: false,
-        locale: Locale(CacheData.lang!),
-        translations: AppLocalization(),
-      home:const KitchenView(),
+      title: ConstantsManager.appTitle,
+      theme: ThemeData(
+        fontFamily: ConstantsManager.fontFamily,
+      ),
+      debugShowCheckedModeBanner: false,
+      locale: Locale(CacheData.lang!),
+      translations: AppLocalization(),
+
     );
   }
 }
+
