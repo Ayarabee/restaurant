0dc17384dc2613a3f51d7b813fc48423bd1c5bde
ayara <ayarabee406@gmail.com>
2023-09-24 14:30:17 +0200

    update master

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       locale: Locale(CacheData.lang!),
       translations: AppLocalization(),
-      home: const AddUserView(),
+
     );
   }
 }
