815508f2cb3103081fd28f2824e8211a3ae0e3be
Heba-Ramzii <hebaramzii33@gmail.com>
2023-09-24 20:45:32 +0300

    add items init

---
+++ lib/feature/items/presentation/views/add_item_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddItemView extends StatelessWidget {
+  const AddItemView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
