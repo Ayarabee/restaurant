cdeab373cdd9824580f16d5050e9a3d098e14f52
Heba-Ramzii <hebaramzii33@gmail.com>
2023-09-24 21:03:14 +0300

    update master55

--- lib/feature/items/presentation/views/add_item_view.dart
+++ lib/feature/items/presentation/views/add_item_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant/core/core_widgets/default_body/default_body.dart';
 
 class AddItemView extends StatelessWidget {
   const AddItemView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  const DefaultBody(
+      title: "إضافة صنف",
+      sideBarBody: Column(),
+      sideBarTitleToDivider:60,
+
+      body: Column(),
+
+    );
   }
 }
