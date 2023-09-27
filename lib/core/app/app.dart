a061269040595f378aaf04956309af8ce8cad9d5
Heba-Ramzii <hebaramzii33@gmail.com>
2023-09-27 03:41:12 +0300

    items_view

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -7,8 +7,7 @@ import 'package:restaurant/core/resources_manager/constants_manager.dart';
 import 'package:restaurant/feature/items/presentation/manager/item_cubit.dart';
 import 'package:restaurant/feature/items/presentation/manager/quantity_cubit.dart';
 
-import '../../feature/items/presentation/views/add_quantity_view.dart';
-import '../../feature/items/presentation/views/item_details_view.dart';
+import '../../feature/items/presentation/views/items_view.dart';
 
 class MyApp extends StatelessWidget {
 
@@ -34,7 +33,8 @@ class MyApp extends StatelessWidget {
         locale: Locale(CacheData.lang!),
         translations: AppLocalization(),
         //home: const AddQuantityView()
-        home: const ItemDetailsView()
+        //home: const ItemDetailsView()
+        home: const ItemsView()
       ),
     );
   }
---
+++ lib/feature/items/presentation/views/widgets/items/side_quick_items_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class SideQuickItemsBody extends StatelessWidget {
+  const SideQuickItemsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        // ItemCodeDropDown(
+        //     text: 'text',
+        //     textEditingController: textEditingController,
+        //     items: 3,
+        //     onChanged:  (){}
+        // ),
+      ],
+    );
+  }
+}
