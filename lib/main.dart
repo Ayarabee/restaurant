342c2f5ee7f491a4fbfaa71d80c4913e828f5507
Ebrahim <hemaaibrahem8@gmail.com>
2024-03-25 18:18:15 +0200

    //kk

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:restaurant/core/local_database/cache_helper.dart';
 import 'core/app/app.dart';
 import 'core/localization/app_localization.dart';
 
-
+//jjjj
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await CacheHelper.init();
